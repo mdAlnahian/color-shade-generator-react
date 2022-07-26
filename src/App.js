dcc60507f94fa7bc22910427a47a53565eacf3fe
mdAlnahian <muhammedalnahian@gmail.com>
2022-07-26 18:53:18 +0600

    color shade done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import Values from 'values.js';
+import { useState } from 'react';
+import SingleColor from './SingleColor';
 
 function App() {
+
+  const [color , setColor] = useState('');
+  const [error , setError] = useState(false);
+  const [list, setList] = useState(new Values('#f14924').all(10));
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+      
+    } catch (error) {
+      setError(true)
+      console.log(error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            placeholder="#f15025"
+            onChange={(e) => setColor(e.target.value)}
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </div>
+      <section className='colors'>
+        <h4>List Goes Here</h4>
+        {
+          list.map((color,index)=>{
+            // console.log(color);
+            return (
+              <SingleColor
+                key={index}
+                {...color}
+                hexColor={color.hex}     index={index}
+              ></SingleColor>
+            );
+          })
+        }
+      </section>
+    </>
   );
 }
 
